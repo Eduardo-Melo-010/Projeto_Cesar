d2bc389da804f50e33ed3721788ab99c8a595aae
Eduardo-Melo-010 <eduardo.smelo@edu.pe.senac.br>
2025-05-06 03:01:55 -0300

    Update CadastroPaciente.jsx

--- CadastroPaciente.jsx
+++ CadastroPaciente.jsx
@@ -21,12 +21,16 @@ function CadastroPaciente() {
               <input type="text" name="nome" placeholder="Insira o nome do paciente" required />
             </label>
 
+            <label>Matrícula ou CPF <span className="obrigatorio">*</span>
+            <input type="text" name="matricula_ou_CPF" placeholder="Insira a matrícula ou o CPF"/>
+            </label>
+
             <label>Data de Nascimento <span className="obrigatorio">*</span>
               <input type="date" name="data_nascimento" required />
             </label>
 
             <label> Nome do Responsável <span className="obrigatorio">*</span>
-              <input type="text" name="sobrenome" placeholder="Nome do Responsável (caso seja menor de idade)" required />
+              <input type="text" name="NomeResponsavel" placeholder="Nome do Responsável (caso seja menor de idade)" required />
             </label>
 
             <label>Telefone <span className="obrigatorio">*</span>
@@ -36,17 +40,16 @@ function CadastroPaciente() {
             <label>E-mail <span className="obrigatorio">*</span>
               <input type="email" name="email" placeholder="Insira seu e-mail" required />
             </label>
-
-            <label>Mensagem <span className="obrigatorio">*</span>
-              <textarea name="mensagem" placeholder="Informe o que deseja." required></textarea>
-            </label>
-
-            <label>Língua
-              <select name="lingua">
-                <option>Português (pt-br)</option>
-                <option>Inglês (en)</option>
+            
+            <label>Tipo de Vínculo
+              <select name="TipoDeVinculo">
+                <option>Aluno</option>
+                <option>Profissinal do Cesar</option>
+                <option>Outros</option>
               </select>
+              <textarea name="mensagem" placeholder="Se a escolha foi outros, informe o tipo de vínculo." required></textarea>
             </label>
+           
 
             <label>Protótipo (Requisitos)
               <input type="file" name="prototipo" />
@@ -102,3 +105,4 @@ function CadastroPaciente() {
 }
 
 export default CadastroPaciente;
+
